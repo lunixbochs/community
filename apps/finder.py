79a647ac2759445c0a1236bb18f930901b0b4a97
Ryan Hileman <lunixbochs@gmail.com>
2019-05-06 08:22:48 -0700

    fix finder.py

--- apps/finder.py
+++ apps/finder.py
@@ -1,4 +1,4 @@
-from talon import Module, actions
+from talon import Module, Context, actions
 
 key = actions.key
 
@@ -7,80 +7,146 @@ key = actions.key
 mod = Module()
 @mod.action_class
 class Actions:
-    def duplicate(): key('cmd-d')
-    def eject():     key('cmd-e')
+    def eject():
+        """Eject the selected volume"""
+        key('cmd-e')
 
-    # FIXME: move these to app actions
-    def hide():        key('cmd-h')
-    def hide_others(): key('cmd-h')
+    def toggle_toolbar():
+        """Show or hide the Finder toolbar"""
+        key('cmd-alt-t')
 
-    def hide_toolbar(): key('cmd-alt-t')
-    def view_options(): key('cmd-j')
-    def connect(): key('cmd-k')
-    def alias(): key('cmd-l')
-    def info(): key('cmd-i')
-    def new_folder(): key('cmd-shift-n')
-    def collapse(): key('cmd-left')
-    def expand(): key('cmd-right')
-    def open(): key('cmd-down')
-    def show_original(): key('cmd-r')
-    def add_to_sidebar(): key('cmd-t')
-    def trash(): key('cmd-backspace')
+    def view_options():
+        'Open "View Options" dialog'
+        key('cmd-j')
 
-# actions
-"select all": Key("cmd-a"),
-"copy": Key("cmd-c"),
-"duplicate": Key("cmd-d"),
-"eject": Key("cmd-e"),
-"(search | find)": Key("cmd-f"),
-"hide [finder]": Key("cmd-h"),
-"hide (others | else)": Key("cmd-alt-h"),
-"(hide | no) toolbar": Key("cmd-alt-t"),
-"info": Key("cmd-i"),
-"view [options]": Key("cmd-j"),
-"connect [to server]": Key("cmd-k"),
-"[(make | create)] (alias | shortcut)": Key("cmd-l"),
-"minimize": Key("cmd-m"),
-"new window": Key("cmd-n"),
-"new folder": Key("cmd-shift-n"),
-# NOT WORKING "new smart folder": Key("cmd-alt-n"),
-"collapse": Key("cmd-left"),
-"expand": Key("cmd-right"),
-"open": Key("cmd-down"),
-"[show] original": Key("cmd-r"),
-"add to side bar": Key("cmd-t"),
-"trash it": Key("cmd-backspace"),
-"new tab": Key("cmd-alt-o"),
-"paste": Key("cmd-v"),
-"close": Key("cmd-w"),
-"cut": Key("cmd-x"),
-"undo": Key("cmd-z"),
-"[finder] preferences": Key("cmd-,"),
-"(icon | icons) [(mode | view)]": Key("cmd-1"),
-"list [(mode | view)]": Key("cmd-2"),
-"(column | columns) [(mode | view)]": Key("cmd-3"),
-"cover [flow] [(mode | view)]": Key("cmd-4"),
-"help": Key("cmd-?"),
-# navigation
-"back": Key("cmd-["),
-"(forward | next)": Key("cmd-]"),
-"(up | (parent [folder]))": Key("cmd-up"),
-"(cycle | switch) [window]": Key("cmd-`"),
-"computer": Key("cmd-shift-c"),
-"desktop": Key("cmd-shift-d"),
-"all files": Key("cmd-shift-f"),
-"go to": Key("cmd-shift-g"),
-"home": Key("cmd-shift-h"),
-"icloud": Key("cmd-shift-i"),
-"documents": Key("cmd-shift-o"),
-"air drop": Key("cmd-shift-r"),
-"utilities": Key("cmd-shift-u"),
-"downloads": Key("cmd-shift-l"),
-"applications": Key("cmd-shift-a"),
-"developer": go_to_path("~/Developer"),
-"talon": go_to_path("~/.talon/user"),
-# NOT WORKING "(delete | empty) trash": Key("cmd-shift-del"),
-"spotlight [menu]": Key("cmd-space"),
-"spotlight window": Key("cmd-alt-space"),
-# NOT WORKING: Function key shorcuts (f8 through f12)
-"toggle hidden files": Key("cmd-shift-."),
+    def connect():
+        'Open "Connect" dialog'
+        key('cmd-k')
+
+    def item_duplicate():
+        """Duplicate selected item"""
+        key('cmd-d')
+
+    def item_alias():
+        """Make an alias of selected item"""
+        key('cmd-l')
+
+    def item_info():
+        """Show info of selected item"""
+        key('cmd-i')
+
+    def item_open():
+        """Open selected item"""
+        key('cmd-down')
+
+    def item_add_to_sidebar():
+        """Add selected item to sidebar"""
+        key('cmd-t')
+
+    def item_trash():
+        """Move selected item to Trash"""
+        key('cmd-backspace')
+
+    def item_show_original():
+        """Show the target of selected alias"""
+        key('ctrl-cmd-alt-a')
+
+    def new_folder():
+        """Create a new folder"""
+        key('cmd-shift-n')
+
+    def collapse():
+        """Collapse current row"""
+        key('cmd-left')
+
+    def expand():
+        """Expand current row"""
+        key('cmd-right')
+
+    def icon_view():
+        'Switch to "Icon" view'
+        key('cmd-1')
+
+    def list_view():
+        'Switch to "List" view'
+        key('cmd-2')
+
+    def column_view():
+        'Switch to "Column" view'
+        key('cmd-3')
+
+    def cover_flow_view():
+        'Switch to "Cover Flow" view'
+        key('cmd-4')
+
+    def help():
+        'Open "Help" menu'
+        key('cmd-?')
+
+    def toggle_hidden_files():
+        "Toggle showing hidden files"
+        key('cmd-shift-.')
+
+    def go_to(path: str=None):
+        "Open the Go To dialog, or go directly to a path"
+        key('cmd-shift-g')
+        if path is not None:
+            actions.insert(path)
+            key('enter')
+
+    def go_computer():
+        'Open "Computer"'
+        key('cmd-shift-c')
+
+    def go_desktop():
+        'Open "Desktop"'
+        key('cmd-shift-d')
+
+    def go_all_files():
+        'Open "All Files"'
+        key('cmd-shift-f')
+
+    def go_home():
+        'Open "Home Directory"'
+        key('cmd-shift-h')
+
+    def go_icloud():
+        'Open "iCloud"'
+        key('cmd-shift-i')
+
+    def go_documents():
+        'Open "Documents"'
+        key('cmd-shift-o')
+
+    def go_airdrop():
+        'Open "AirDrop"'
+        key('cmd-shift-r')
+
+    def go_utilities():
+        'Open "Utilities"'
+        key('cmd-shift-u')
+
+    def go_downloads():
+        'Open "Downloads"'
+        key('cmd-shift-l')
+
+    def go_applications():
+        'Open "Applications"'
+        key('cmd-shift-a')
+
+    def go_talon():
+        'Open "~/.talon"'
+        actions.self.go_to('~/.talon')
+
+    def go_talon_user():
+        'Open "~/.talon/user"'
+        actions.self.go_to('~/.talon/user')
+
+ctx = Context()
+ctx.on({'app.bundle': 'com.apple.Finder'})
+
+@ctx.action_class('app')
+class AppActions:
+    def window_close(): key('cmd-shift-w')
+    def tab_open():  key('cmd-t')
+    def tab_close(): key('cmd-w')
